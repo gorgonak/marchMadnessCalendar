a17cd87eb8c779e434443bbed0c7563d35bd4a31
gorgonak <59940735+gorgonak@users.noreply.github.com>
2021-03-27 03:16:01 -0700

    Add files via upload
    Adding initial code, working with 4 pictures.

---
+++ main.py
@@ -0,0 +1,12 @@
+from picture import *
+
+root = Tk()
+root.title('March Madness')
+
+WAF = Pic("waffle", 0, 0)
+PI = Pic("pi", 0, 1)
+STP = Pic("stp", 1, 0)
+MAR = Pic("mario", 1, 1)
+JOE = Pic("joe", 0, 2)
+
+root.mainloop()
---
+++ picture.py
@@ -0,0 +1,36 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import time
+
+
+class Pic:
+    def __init__(self, imgname, column, row):
+
+        self.img = imgname
+        self.col = column
+        self.row = row
+
+        self.image = self.img + ".png"
+        self.alt_image = self.img + "_fact.png"
+        self.img_src = Image.open("img\\" + self.image)
+        self.alt_img_src = Image.open("img\\" + self.alt_image)
+        self.pic = ImageTk.PhotoImage(self.img_src)
+        self.alt_pic = ImageTk.PhotoImage(self.alt_img_src)
+
+        self.Button = Button(image=self.pic, command=self.big_b, height=150, width=150)
+        self.Button.grid(column=self.col, row=self.row)
+
+    def big_b(self):
+        self.Button.destroy()
+        print(self.img + ".Button Destroyed")
+        self.BigButton = Button(image=self.alt_pic, command=self.little_b)
+        self.BigButton.grid(column=self.col, row=self.row)
+        print(self.img + " clicked")
+
+    def little_b(self):
+        self.BigButton.destroy()
+        print(self.img + ".BigButton Destroyed")
+
+        self.Button = Button(image=self.pic, command=self.big_b, height=150, width=150)
+        self.Button.grid(column=self.col, row=self.row)
+        print(self.img + " clicked")
